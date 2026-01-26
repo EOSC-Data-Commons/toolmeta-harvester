a6117b8960184b8dd66c3a8bbaf5d1c88890b4e8
Reggie <reggie.cushing@dans.knaw.nl>
2026-01-26 13:48:46 +0100

    update models

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 .PHONY: run-local
-run-local:
+run-local: postgres
 	uv run src/toolmeta_harvester/main.py
+
+postgres:
+	docker compose up -d postgres
 
 .PHONY: sync
 sync:
--- src/toolmeta_harvester/db/models.py
+++ src/toolmeta_harvester/db/models.py
@@ -16,37 +16,43 @@ from sqlalchemy.dialects.postgresql import ARRAY
 import string
 import secrets
 
+
 def generate_alphanum_id(length=9):
     chars = string.ascii_lowercase + string.digits
-    return ''.join(secrets.choice(chars) for _ in range(length))
+    return "".join(secrets.choice(chars) for _ in range(length))
+
 
 def generate_tool_id():
     return f"edc:tool:{generate_alphanum_id()}"
 
 
-
 Base = declarative_base()
 
-class Repository(Base):
-    __tablename__ = "repositories"
 
-    id = Column(Integer,
-                Identity(start=1),
-                autoincrement=True,
-                primary_key=True)           # logical repository id
-    url = Column(Text, nullable=False, index=True)  # repository URL
-    no_tools = Column(Integer)                  # number of tools found
-    status = Column(Text, nullable=False)        # active, archived, deleted, ...
-    eror_code = Column(Text)                      # HTTP error code if applicable
+class ArtifactHarvest(Base):
+    __tablename__ = "artifact_harvests"
 
-    source_type = Column(Text, nullable=False)    # galaxy, cwl, nextflow, ...
-
+    id = Column(
+        Integer, Identity(start=1), autoincrement=True, primary_key=True
+    )  # logical repository id
+    url = Column(Text, nullable=False, index=True)
+    # pending, error, processing, completed
+    status = Column(Text, nullable=False)
+    # HTTP error code if applicable
+    eror_code = Column(Text)
+    # usegalaxy.org, workflowhub.eu, ...
+    source_type = Column(Text, nullable=False)
+    # galaxy_workflow, galaxy_tool, cwl_tool, nextflow_pipeline, ...
+    artifact_type = Column(Text, nullable=False)
+    # id of the stored artifact in the corresponding table
+    stored_id = Column(Text, nullable=False)
+    # name of the corresponding table where the artifact is stored
+    stored_table = Column(Text, nullable=False)
     created_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),
         nullable=False,
     )
-
     updated_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),
@@ -54,24 +60,77 @@ class Repository(Base):
         nullable=False,
     )
 
-class Tool(Base):
-    __tablename__ = "tools"
 
-    id = Column(String, primary_key=True, default=generate_tool_id, unique=True, nullable=False)           # logical tool id
+class GalaxyWorkflow(Base):
+    __tablename__ = "galaxy_workflows"
+
+    id = Column(
+        String, primary_key=True, unique=True, nullable=False
+    )  # logical workflow id
+    name = Column(Text, nullable=False)
+    description = Column(Text)
+    url = Column(Text, nullable=False, index=True)
+    version = Column(Text)
+    input_toolshed_tools = Column(ARRAY(Text))
+    output_toolshed_tools = Column(ARRAY(Text))
+    toolshed_tools = Column(ARRAY(Text))
+    tags = Column(ARRAY(Text))
+    raw_ga = Column(LargeBinary, nullable=False)  # raw JSON
+    created_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
+
+
+# class Repository(Base):
+#     __tablename__ = "repositories"
+#
+#     id = Column(
+#         Integer, Identity(start=1), autoincrement=True, primary_key=True
+#     )  # logical repository id
+#     url = Column(Text, nullable=False, index=True)  # repository URL
+#     no_tools = Column(Integer)  # number of tools found
+#     # active, archived, deleted, ...
+#     status = Column(Text, nullable=False)
+#     # HTTP error code if applicable
+#     eror_code = Column(Text)
+#
+#     source_type = Column(Text, nullable=False)  # galaxy, cwl, nextflow, ...
+#
+#     created_at = Column(
+#         DateTime(timezone=True),
+#         server_default=func.now(),
+#         nullable=False,
+#     )
+#
+#     updated_at = Column(
+#         DateTime(timezone=True),
+#         server_default=func.now(),
+#         onupdate=func.now(),
+#         nullable=False,
+#     )
+#
+#
+class ShedTool(Base):
+    __tablename__ = "shed_tools"
+
+    id = Column(String, primary_key=True, unique=True, nullable=False)
     name = Column(Text, nullable=False)
     version = Column(Text)
 
-    source_type = Column(Text, nullable=False)    # galaxy, cwl, nextflow, ...
+    source_type = Column(Text, nullable=False)  # galaxy, cwl, nextflow, ...
     source_url = Column(Text)
 
-    description = Column(Text) 
+    description = Column(Text)
     categories = Column(ARRAY(Text))
     owner = Column(Text)
-
-    command = Column(LargeBinary)                         # extracted executable command
-
-    raw = Column(LargeBinary, nullable=False)      # raw XML/YAML/JSON/etc.
-    raw_format = Column(Text, nullable=False)      # xml, yaml, json, nf, ...
 
     created_at = Column(
         DateTime(timezone=True),
@@ -86,32 +145,31 @@ class Tool(Base):
     )
 
     inputs = relationship(
-        "ToolInput",
+        "ShedToolInput",
         back_populates="tool",
         cascade="all, delete-orphan",
     )
 
     outputs = relationship(
-        "ToolOutput",
+        "ShedToolOutput",
         back_populates="tool",
         cascade="all, delete-orphan",
     )
     __table_args__ = (
-        UniqueConstraint('name', 'version', 'source_type', name='uq_tool_name_version_source'),
+        UniqueConstraint(
+            "name", "version", "source_type", name="uq_tool_name_version_source"
+        ),
     )
+
 
 class ToolInput(Base):
-    __tablename__ = "tool_inputs"
-    
-    id = Column(Integer,
-                Identity(start=1),
-                autoincrement=True,
-                primary_key=True)           # logical output id
+    __tablename__ = "shed_tool_inputs"
 
-    tool_id = Column(
-        String,
-        ForeignKey("tools.id", ondelete="CASCADE")
-    )
+    id = Column(
+        Integer, Identity(start=1), autoincrement=True, primary_key=True
+    )  # logical output id
+
+    tool_id = Column(String, ForeignKey("shed_tools.id", ondelete="CASCADE"))
     name = Column(Text, nullable=True)
     type = Column(Text, nullable=True)
     # format = Column(Text, nullable=True)
@@ -123,7 +181,7 @@ class ToolInput(Base):
         server_default=func.now(),
         nullable=False,
     )
-    
+
     updated_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),
@@ -131,34 +189,29 @@ class ToolInput(Base):
         nullable=False,
     )
 
-    tool = relationship("Tool", back_populates="inputs")
-    
+    tool = relationship("ShedTool", back_populates="inputs")
 
 
-class ToolOutput(Base):
-    __tablename__ = "tool_outputs"
+class ShedToolOutput(Base):
+    __tablename__ = "shed_tool_outputs"
 
-    id = Column(Integer,
-                Identity(start=1),
-                autoincrement=True,
-                primary_key=True)           # logical output id
+    id = Column(
+        Integer, Identity(start=1), autoincrement=True, primary_key=True
+    )  # logical output id
 
-    tool_id = Column(
-        String,
-        ForeignKey("tools.id", ondelete="CASCADE")
-    )
+    tool_id = Column(String, ForeignKey("shed_tools.id", ondelete="CASCADE"))
     name = Column(Text, nullable=True)
     type = Column(Text, nullable=True)
     # format = Column(Text, nullable=False)
     format = Column(ARRAY(Text), nullable=True)
     label = Column(Text, nullable=True)
-    
+
     created_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),
         nullable=False,
     )
-    
+
     updated_at = Column(
         DateTime(timezone=True),
         server_default=func.now(),
@@ -166,6 +219,4 @@ class ToolOutput(Base):
         nullable=False,
     )
 
-
-    tool = relationship("Tool", back_populates="outputs")
-
+    tool = relationship("ShedTool", back_populates="outputs")

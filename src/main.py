f54e31c173cf1e0d365b70d34dfa1acbea5d87e1
Reggie Cushing <r.cushing@esciencecenter.nl>
2026-02-28 13:00:26 +0100

    Adding file logging

--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
 import logging
+from pathlib import Path
 from toolmeta_harvester.flows import harvest_galaxy_hub_workflows as gwh
 
+LOG_FILE = Path("logs/harvest_galaxy_hub_workflows.log")
+# Create directory if it does not exist
+LOG_FILE.parent.mkdir(parents=True, exist_ok=True)
+
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="%(asctime)s %(name)s %(levelname)s: %(message)s",
+    handlers=[logging.StreamHandler(),
+              logging.FileHandler(LOG_FILE)],
 )
 
 logger = logging.getLogger(__name__)

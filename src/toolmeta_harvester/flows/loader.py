9263affec16f0b93fc07c059f549704721c619ec
Reggie Cushing <reggie.cushing@dans.knaw.nl>
2026-08-29 14:01:37 +0200

    Adding flow/loader.py

---
+++ src/toolmeta_harvester/flows/loader.py
@@ -0,0 +1,8 @@
+# toolmeta_harvester/flows/loader.py
+
+
+def load_flows() -> None:
+    from toolmeta_harvester.flows import harvest_github  # noqa: F401
+    from toolmeta_harvester.flows import harvest_workflowhub  # noqa: F401
+    from toolmeta_harvester.flows import harvest_zenodo  # noqa: F401
+    from toolmeta_harvester.flows import harvest_bioschemas  # noqa: F401

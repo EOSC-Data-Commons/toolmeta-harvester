73b86ab792730ca2963f2dfb26f9e99c962e8b7c
Reggie Cushing <reggie.cushing@dans.knaw.nl>
2026-08-30 10:36:12 +0200

    Adding test static dags to harvest only 3 records

--- airflow_dags/static_harvests.py
+++ airflow_dags/static_harvests.py
@@ -14,6 +14,31 @@ def create_static_dag(flow):
     def harvest_dag():
         @task
         def harvest():
+            result = flow.handler()
+
+            return {
+                "pipeline_tag": result.pipeline_tag,
+                "record_ids": result.record_ids,
+                "harvested_count": result.harvested_count,
+                "failed_count": result.failed_count,
+            }
+
+        harvest()
+
+    return harvest_dag()
+
+
+def create_test_static_dag(flow):
+    @dag(
+        dag_id=f"harvest_{flow.name}_test",
+        schedule=flow.default_schedule,
+        catchup=False,
+        max_active_runs=1,
+        tags=["tool-harvester", "static"],
+    )
+    def harvest_dag():
+        @task
+        def harvest():
             result = flow.handler(limit=3)
 
             return {
@@ -31,3 +56,5 @@ def create_static_dag(flow):
 for flow in get_static_flows():
     dag_id = f"harvest_{flow.name}"
     globals()[dag_id] = create_static_dag(flow)
+    dag_id_test = f"harvest_{flow.name}_test"
+    globals()[dag_id_test] = create_test_static_dag(flow)

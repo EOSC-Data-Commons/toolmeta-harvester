d5a1d3789c7a5ed11471db2acb5faf4944704beb
Reggie Cushing <reggie.cushing@dans.knaw.nl>
2026-09-01 16:51:58 +0200

    feat(toolmeta_harvester): add harvest_rsd to flows

--- src/toolmeta_harvester/flows/loader.py
+++ src/toolmeta_harvester/flows/loader.py
@@ -6,3 +6,4 @@ def load_flows() -> None:
     from toolmeta_harvester.flows import harvest_workflowhub  # noqa: F401
     from toolmeta_harvester.flows import harvest_zenodo  # noqa: F401
     from toolmeta_harvester.flows import harvest_bioschemas  # noqa: F401
+    from toolmeta_harvester.flows import harvest_rsd  # noqa: F401
--- src/toolmeta_harvester/scripts/dump_flows.py
+++ src/toolmeta_harvester/scripts/dump_flows.py
@@ -4,6 +4,7 @@ import toolmeta_harvester.flows.harvest_zenodo  # noqa: F401
 import toolmeta_harvester.flows.harvest_github  # noqa: F401
 import toolmeta_harvester.flows.harvest_bioschemas  # noqa: F401
 import toolmeta_harvester.flows.harvest_workflowhub  # noqa: F401
+import toolmeta_harvester.flows.harvest_rsd  # noqa: F401
 
 from toolmeta_harvester.flows.registry import (
     get_flows,
